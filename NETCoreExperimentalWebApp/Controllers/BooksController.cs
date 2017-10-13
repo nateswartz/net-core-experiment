9f1e1df847cdfe8f96cf9a1273bf5da0920fec61
Nate Swartzentruber <nswartzentruber@gmail.com>
2017-10-12 20:52:22 -0400

    Clean up default scaffolding
    Remove unused views and controller actions

--- NETCoreExperimentalWebApp/Controllers/BooksController.cs
+++ NETCoreExperimentalWebApp/Controllers/BooksController.cs
@@ -21,11 +21,6 @@ namespace NETCoreExperimentalWebApp.Controllers
             return View(_bookData.GetAll());
         }
 
-        public JsonResult GetIndex()
-        {
-            return Json(_bookData.GetAll());
-        }
-
         // GET: Books/Details/5
         public IActionResult Details(int? id)
         {
@@ -43,26 +38,8 @@ namespace NETCoreExperimentalWebApp.Controllers
             return View(book);
         }
 
-        // GET: Books/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Books/Create
         [HttpPost]
-        public IActionResult Create([Bind("id,Title,Author,Type,Image")] BookModel book)
-        {
-            if (ModelState.IsValid)
-            {
-                _bookData.Create(book);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(book);
-        }
-
-        [HttpPost]
-        public IActionResult CreateViaJSON([FromBody]BookModel book)
+        public IActionResult Create([FromBody]BookModel book)
         {
             book.id = 0;
             if (ModelState.IsValid)
@@ -73,43 +50,8 @@ namespace NETCoreExperimentalWebApp.Controllers
             return BadRequest("Book data incomplete");
         }
 
-        // GET: Books/Edit/5
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var book = _bookData.Get(id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-            return View(book);
-        }
-
-        // POST: Books/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public IActionResult Edit(int id, [Bind("id,Title,Author,Type,Image")] BookModel book)
-        {
-            if (id != book.id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                _bookData.Update(id, book);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(book);
-        }
-
-        [HttpPost]
-        public IActionResult EditViaJSON(int id, [FromBody]BookModel book)
+        public IActionResult Edit(int id, [FromBody]BookModel book)
         {
             if (ModelState.IsValid)
             {
@@ -119,26 +61,9 @@ namespace NETCoreExperimentalWebApp.Controllers
             return BadRequest("Book data incomplete");
         }
 
-        // GET: Books/Delete/5
-        public IActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var book = _bookData.Get(id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            return View(book);
-        }
-
         // POST: Books/Delete/5
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int id)
+        [HttpPost]
+        public IActionResult Delete(int id)
         {
             _bookData.Delete(id);
             return RedirectToAction(nameof(Index));
