b792fcdcfc553ba8e2b235c87fffbe58be6a9736
Nate Swartzentruber <nswartzentruber@gmail.com>
2017-10-20 13:30:08 -0400

    Clean up some of the JS on the Books Page
    Add knockout-mapping library to simplify KO boilerplate
    Move BookViewModel to separate file

---
+++ NETCoreExperimentalWebApp/wwwroot/js/KOModels/BookViewModel.js
@@ -0,0 +1,8 @@
+﻿function BookViewModel(id, author, title, image, type) {
+    var self = this;
+    self.id = ko.observable(id);
+    self.Author = ko.observable(author);
+    self.Title = ko.observable(title);
+    self.Image = ko.observable(image);
+    self.Type = ko.observable(type);
+};
