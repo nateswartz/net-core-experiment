4c3918305be044e5bb793b4379fc5913970e04e5
Nate Swartzentruber <nswartzentruber@gmail.com>
2017-10-30 13:25:28 -0400

    Re-introduce StarshipViewModel, actually use it

---
+++ NETCoreExperimentalWebApp/wwwroot/js/KOModels/StarshipViewModel.js
@@ -0,0 +1,14 @@
+﻿function StarshipViewModel(data) {
+    var self = this;
+    self.Name = ko.observable(data.Name);
+    self.Model = ko.observable(data.Model);
+    self.Manufacturer = ko.observable(data.Manufacturer);
+    self.Cost = ko.observable(data.Cost_In_Credits);
+    self.Length = ko.observable(data.Length);
+    self.HyperdriveRating = ko.observable(data.Hyperdrive_Rating);
+    self.MGLT = ko.observable(data.MGLT);
+
+    self.FormattedCost = ko.computed(function () {
+        return self.Cost().toLocaleString({ useGrouping: true });
+    }, self);
+};
