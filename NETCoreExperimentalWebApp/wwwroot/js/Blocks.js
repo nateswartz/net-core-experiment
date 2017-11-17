d15fdd6814b8382d6623ba3f05095aea9ca59a0f
Nate Swartzentruber <nswartzentruber@gmail.com>
2017-11-17 12:57:18 -0500

    Continue wiring up Blockly Editor

--- NETCoreExperimentalWebApp/wwwroot/js/Blocks.js
+++ NETCoreExperimentalWebApp/wwwroot/js/Blocks.js
@@ -11,6 +11,24 @@
 
 Blockly.JavaScript['icon_change_color_red'] = function (block) {
     // String or array length.
-    var code = "$('#targetElement').addClass('red');";
+    var code = "$('#targetElement').delay(500).addClass('red');";
+    return [code, Blockly.JavaScript.ORDER_MEMBER];
+};
+
+
+Blockly.Blocks['icon_change_color_green'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField('change color to green');
+        this.setColour(160);
+        this.setTooltip('Change the color of the icon');
+        this.setNextStatement(true);
+        this.setPreviousStatement(true);
+    }
+};
+
+Blockly.JavaScript['icon_change_color_green'] = function (block) {
+    // String or array length.
+    var code = "$('#targetElement').delay(500).addClass('green');";
     return [code, Blockly.JavaScript.ORDER_MEMBER];
 };
