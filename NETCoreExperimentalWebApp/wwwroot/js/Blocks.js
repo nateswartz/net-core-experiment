a2d5bdc171eb91eba318176565853f167abf7946
Nate Swartzentruber <nswartzentruber@gmail.com>
2017-11-24 15:18:53 -0500

    Clean up and extend Blockly editor

--- NETCoreExperimentalWebApp/wwwroot/js/Blocks.js
+++ NETCoreExperimentalWebApp/wwwroot/js/Blocks.js
@@ -1,36 +1,26 @@
-﻿Blockly.Blocks['icon_change_color_red'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField('change color to red');
-    this.setColour(160);
-    this.setTooltip('Change the color of the icon');
-    this.setNextStatement(true);
-    this.setPreviousStatement(true);  
-  }
-};
-
-Blockly.JavaScript['icon_change_color_red'] = function (block) {
-    var timer = 500 * Timer.count;
-    Timer.count++;
-    var code = "setTimeout(function(){$('#targetElement').css('color', 'red');}, " + timer + ");";
-    return [code, Blockly.JavaScript.ORDER_MEMBER];
-};
-
-
-Blockly.Blocks['icon_change_color_green'] = {
+﻿Blockly.Blocks['icon_change_color'] = {
     init: function () {
+        var color = new Blockly.FieldColour('#000000');
+        color.setColours(['#f00', '#0f0', '#00f', '#f0f', '#000', '#fff']).setColumns(3);
         this.appendDummyInput()
-            .appendField('change color to green');
+            .appendField('Change:')
+            .appendField(new Blockly.FieldDropdown([
+                ['house', 'House'],
+                ['speech bubble', 'Talk']
+            ]), 'ITEM');
+        this.appendDummyInput()
+            .appendField(' color to:')
+            .appendField(color, 'COLOR');
         this.setColour(160);
-        this.setTooltip('Change the color of the icon');
+        this.setTooltip('Change the color of the selected item');
+        this.setInputsInline(true);
         this.setNextStatement(true);
         this.setPreviousStatement(true);
     }
 };
 
-Blockly.JavaScript['icon_change_color_green'] = function (block) {
-    var timer = 500 * Timer.count;
-    Timer.count++;
-    var code = "setTimeout(function(){$('#targetElement').css('color', 'green');}, " + timer + ");";
-    return [code, Blockly.JavaScript.ORDER_MEMBER];
+Blockly.JavaScript['icon_change_color'] = function (block) {
+    var code = "$('#icon" + block.getFieldValue('ITEM') + "')" +
+                ".css('color', '" + block.getFieldValue('COLOR') + "');";
+    return code;
 };
